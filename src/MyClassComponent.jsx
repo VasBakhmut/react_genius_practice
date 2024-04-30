9c860a39198b3634ced4e39fe4e978000ddc36e1
Vas Bakhmut <mizaracnc@gmail.com>
2024-04-30 10:09:17 +1000

    first

---
+++ src/MyClassComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class MyClassComponent extends Component {
+	render() {
+		return (
+			<div>
+				<ul>
+					<li>1</li>
+					<li>2</li>
+					<li>3</li>
+				</ul>
+			</div>
+		)
+	}
+}
